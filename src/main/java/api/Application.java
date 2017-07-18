5bb61cf2aae27f35a3cf6791aa76874d30b2d1e2
Junghwan Park <reserve.dev@gmail.com>
2017-07-18 23:35:50 +0900

    modify package name and fix error

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -1,4 +1,4 @@
-package demo;
+package api;
 
 import com.google.gson.Gson;
 import static spark.Spark.*;
@@ -12,7 +12,7 @@ public class Application {
 		port(8080);
 
 		get("/api", (req, res) -> {
-			String query = req.queryParams("query") == null ? "바람과 함께 사라지다를 봤어." : req.queryParams("sentence");
+			String query = req.queryParams("query") == null ? "바람과 함께 사라지다를 봤어." : req.queryParams("query");
 			String modelType = req.queryParams("model_type") == null ? "full" : req.queryParams("model_type");
 
 			komoran.setUserInput(query, modelType);
--- src/main/java/api/KOMORANDemo.java
+++ src/main/java/api/KOMORANDemo.java
@@ -1,4 +1,4 @@
-package demo;
+package api;
 
 import java.util.List;
 
